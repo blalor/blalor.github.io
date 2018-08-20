0ccf20c5fb553d9534520f99b61415e8b98ce134
Brian Lalor <blalor@bravo5.org>
2018-08-20 14:43:29 -0400

    make bucket value sensitive

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,9 @@
 output "bucket" {
     value = "${aws_s3_bucket.site_bucket.id}"
+    ## just to prevent it from being printed to the console
+    sensitive = true
 }
 
 output "jekyll_site_prefix" {
     value = "${local.jekyll_site_prefix}"
-}
-
-output "api_gw_base_url" {
-    value = "${aws_api_gateway_deployment.main.invoke_url}"
 }
